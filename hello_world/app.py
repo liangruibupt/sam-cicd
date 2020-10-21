3729c1bda7a8d6c3ec5b70bf554d006dafee7902
liangrui <liangrui.bupt@gmail.com>
2020-10-21 23:40:29 +0000

    update app.py code

--- hello_world/app.py
+++ hello_world/app.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world! canary " + dt_string,
+            "message": "hello world! Latest canary " + dt_string,
             "location": ip.text.replace("\n", "")
         }),
     }
